127f5b6f5cebfbff40de5b85d248b2c1139f8a2f
Artem <grekartemvl@mail.ru>
2022-12-20 18:41:26 +0500

    Add Task15

---
+++ Task13/Program.cs
@@ -0,0 +1,25 @@
+﻿// 13. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5  78 -> третьей цифры нет  32679 -> 6
+
+Console.Clear();
+
+int FindThirdDigit(int num)
+{
+	int result;
+	if(num > 99)
+   {
+		int ed = num % 10;
+    }
+	result = ed;
+}
+
+Console.Write("Input a two-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int thirdDigit = FindThirdDigit(number);
+Console.WriteLine($"Third Digit of {number} is {thirdDigit}");
+
+int n = int.Parse(Console.ReadLine());
+int k = (int)Math.Log10(n)-2;
+Console.WriteLine(k < 0 ? "No such digit" : (n % (int)Math.Pow(10, k + 1) / (int)Math.Pow(10, k)).ToString());
+Console.ReadLine();
---
+++ Task15/Program.cs
@@ -0,0 +1,21 @@
+﻿// 15. Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да  7 -> да  1 -> нет
+
+Console.Clear();
+
+Console.Write("Введите цифру, обозначающую день недели: ");
+int dayNumber = int.Parse(Console.ReadLine()!);
+
+void CheckingTheDayOfTheWeek(int dayNumber)
+{
+    if (dayNumber == 6 || dayNumber == 7)
+    {
+        Console.WriteLine("Этот день выходной -> да");
+    }
+    else if (dayNumber < 1 || dayNumber > 7)
+    {
+        Console.WriteLine("Это вообще не день недели");
+    }
+    else Console.WriteLine("Этот день не выходной -> нет");
+}
+CheckingTheDayOfTheWeek(dayNumber);
